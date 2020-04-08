bbfe7c38487ff976e3d92dd849c94993cf3e3a76
Gersard <gerardo.mascayano@gmail.com>
2020-04-08 15:46:51 -0400

    - Se crea función para insertar imágen en directorio publico

--- app/src/main/java/cl/gerardomascayano/miscocktails/ui/camera/CameraActivity.kt
+++ app/src/main/java/cl/gerardomascayano/miscocktails/ui/camera/CameraActivity.kt
@@ -1,14 +1,24 @@
 package cl.gerardomascayano.miscocktails.ui.camera
 
 import android.Manifest
+import android.content.ContentValues
+import android.content.Intent
 import android.content.pm.PackageManager
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import cl.gerardomascayano.miscocktails.databinding.ActivityCameraBinding
 import com.otaliastudios.cameraview.CameraListener
 import com.otaliastudios.cameraview.PictureResult
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStream
 
 class CameraActivity : AppCompatActivity() {
 
@@ -26,7 +36,7 @@ class CameraActivity : AppCompatActivity() {
     private fun listenPictureTaking() {
         viewBinding.viewCamera.addCameraListener(object : CameraListener() {
             override fun onPictureTaken(result: PictureResult) {
-//                result.tofile
+                result.toBitmap { saveImage(it!!) }
             }
         })
     }
@@ -45,14 +55,32 @@ class CameraActivity : AppCompatActivity() {
 
     override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == REQUEST_PERMISSION){
-
+        if (requestCode == REQUEST_PERMISSION) {
+            takePicture()
         }
     }
 
     private fun isPermissionGranted(): Boolean =
         ContextCompat.checkSelfPermission(this, Manifest.permission_group.STORAGE) == PackageManager.PERMISSION_GRANTED &&
                 ContextCompat.checkSelfPermission(this, Manifest.permission_group.CAMERA) == PackageManager.PERMISSION_GRANTED
+
+    private fun saveImage(bitmap: Bitmap) {
+        val fos: OutputStream?
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val contentValues = ContentValues()
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, "fotoprueba.jpeg")
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, Environment.DIRECTORY_PICTURES)
+            val imageUri = contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            fos = contentResolver.openOutputStream(imageUri!!)
+        } else {
+            val dir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES).toString()
+            val imageFile = File(dir, "fotoprueba.jpeg")
+            fos = FileOutputStream(imageFile)
+        }
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 90, fos)
+    }
 
     companion object {
         const val REQUEST_PERMISSION = 100
